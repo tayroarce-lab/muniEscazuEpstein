ce31e1470df6d5dbe03208513fcb3b34cc8b42f4
ElvisMAM05 <eaguilar119500862@gmail.com>
2026-02-16 15:28:10 -0600

    Otro mini update

---
+++ public/server/serviceViales.js
@@ -0,0 +1,60 @@
+const URL = "http://localhost:3001/viales";
+
+// GET
+async function getViales() {
+    try {
+        const respuestaServidor = await fetch(URL);
+        return await respuestaServidor.json();
+    } catch (error) {
+        console.error("Error al obtener los viales", error);
+    }
+}
+
+// POST
+async function postViales(vial) {
+    try {
+        const respuesta = await fetch(URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(vial)
+        });
+
+        return await respuesta.json();
+    } catch (error) {
+        console.error("Error al crear el vial", error);
+    }
+}
+
+// DELETE
+async function deleteViales(id) {
+    try {
+        const respuesta = await fetch(`${URL}/${id}`, {
+            method: "DELETE"
+        });
+
+        return await respuesta.json();
+    } catch (error) {
+        console.error("Error al eliminar el vial", error);
+    }
+}
+
+// PUT
+async function putViales(id, vial) {
+    try {
+        const respuesta = await fetch(`${URL}/${id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(vial)
+        });
+
+        return await respuesta.json();
+    } catch (error) {
+        console.error("Error al actualizar el vial", error);
+    }
+}
+
+export { getViales, postViales, deleteViales, putViales };
